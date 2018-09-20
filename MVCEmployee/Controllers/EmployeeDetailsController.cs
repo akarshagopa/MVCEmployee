e5eb4fd2fddc15ec04d654e299478bd22c3b9033
Gopa <akarsha.gopa@epsilon.com>
2018-09-20 21:51:20 +0530

    Add project files.

---
+++ MVCEmployee/Controllers/EmployeeDetailsController.cs
@@ -0,0 +1,30 @@
+﻿using MVCEmployee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEmployee.Controllers
+{
+    public class EmployeeDetailsController : Controller
+    {
+        // GET: EmployeeDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EmpDetails()
+        {
+            EmployeeDetails employee = new EmployeeDetails()
+            {
+                Employee_ID = "711546",
+                Employee_Name = "Akarsha",
+                Employee_Address = "Bangalore",
+                Employee_Salary = 33333
+            };
+            return View(employee);
+        }
+    }
+}
---
+++ MVCEmployee/Models/EmployeeDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCEmployee.Models
+{
+    public class EmployeeDetails
+    {
+        public string Employee_ID { get; set; }
+        public string Employee_Name { get; set; }
+        public string Employee_Address { get; set; }
+        public int Employee_Salary { get; set; }
+    }
+}
